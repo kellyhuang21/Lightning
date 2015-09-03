f8f8c05c2d2ce973fe6ccc9b853559068edf397a
unknown <KEHUANG6@697-334-Harlond.sf.k12.internal>
2015-09-03 10:44:09 -0700

    lightning

--- Lightning.pde
+++ Lightning.pde
@@ -3,27 +3,41 @@ int startY=0;
 int endX=0; 
 int endY=300;
 int count;
+double randomLightning;
 
 void setup()
 {
   size(300,300);
   background(0);
-  strokeWeight(3);
-  frameRate(10);
+  strokeWeight(2);
+  frameRate(5);
 
 }
 void draw()
 {
 
+	startX=150; 
+	startY=0; 
+	endX=0; 
+	endY=150;
+	fill(0);
+	rect(0,0,width,height);
 	lightning();
-	lightning2();
-	// startX=150; 
-	// startY=0; 
-	// endX=0; 
-	// endY=150;
-	// fill(0);
-	// rect(0,0,width,height);
-	// lightning();	
+	if (Math.random() <.5){
+	startX=140; 
+	startY=0; 
+	endX=0; 
+	endY=150;
+	lightning();
+}
+	randomLightning = Math.random();
+	if (randomLightning <.2){
+	startX=160; 
+	startY=0; 
+	endX=0; 
+	endY=150;
+	lightning();
+}
 	
 }
 void mousePressed()
@@ -50,19 +64,6 @@ void lightning(){
 		startX=endX; 
 		startY=endY;
 	
-}
-}
-void lightning2(){
-	
-	int i=0;
-     	while(endY<300){
-		endX=startX+(int)(Math.random()*20)-10;
-		endY=startY+(int)(Math.random()*9);
 
-		stroke(195, (int)(Math.random()*145)+110, 55);
-		line(startX, startY, endX, endY);
-		startX=endX; 
-		startY=endY;
-	
-}
+	}
 }
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -19,27 +19,41 @@ int startY=0;
 int endX=0; 
 int endY=300;
 int count;
+double randomLightning;
 
 public void setup()
 {
   size(300,300);
   background(0);
-  strokeWeight(3);
-  frameRate(10);
+  strokeWeight(2);
+  frameRate(5);
 
 }
 public void draw()
 {
 
+	startX=150; 
+	startY=0; 
+	endX=0; 
+	endY=150;
+	fill(0);
+	rect(0,0,width,height);
 	lightning();
-	lightning2();
-	// startX=150; 
-	// startY=0; 
-	// endX=0; 
-	// endY=150;
-	// fill(0);
-	// rect(0,0,width,height);
-	// lightning();	
+	if (Math.random() <.5f){
+	startX=140; 
+	startY=0; 
+	endX=0; 
+	endY=150;
+	lightning();
+}
+	randomLightning = Math.random();
+	if (randomLightning <.2f){
+	startX=160; 
+	startY=0; 
+	endX=0; 
+	endY=150;
+	lightning();
+}
 	
 }
 public void mousePressed()
@@ -66,21 +80,8 @@ public void lightning(){
 		startX=endX; 
 		startY=endY;
 	
-}
-}
-public void lightning2(){
-	
-	int i=0;
-     	while(endY<300){
-		endX=startX+(int)(Math.random()*20)-10;
-		endY=startY+(int)(Math.random()*9);
 
-		stroke(195, (int)(Math.random()*145)+110, 55);
-		line(startX, startY, endX, endY);
-		startX=endX; 
-		startY=endY;
-	
-}
+	}
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Lightning" };
